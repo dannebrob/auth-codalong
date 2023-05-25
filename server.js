f729887e79eead4539b007b00f1f783348e6a88f
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-05-25 21:49:35 +0200

    added user endpoint and mongoose modal 'User'

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1/authCodeAlong';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+});
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -19,6 +40,19 @@ app.use(express.json());
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello Technigo!');
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password: bcrypt.hashSync(password) });
+    const saved = await user.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: 'Could not create user', errors: err.errors });
+  }
 });
 
 app.get('/secrets', (req, res) => {
